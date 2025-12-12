625e157d09307936f566cfe1e89854811e7d2601
DioTzan <dit24203@go.uop.gr>
2025-12-12 19:50:25 +0200

    Add files via upload

---
+++ dd prokect/dd_addorger.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<dd1.h>
+
+typedef struct orderProducts {
+    int productCode;
+    int quantity;                 
+    int first;
+} orderProducts;
+
+void addorder(){
+    int clientCode;
+    char businessName[NAME_LEN];
+    products
+    
+
+    int productCode;               
+    int quantity;                 
+    int first;      
+
+
+    orderProducts orderProduct[]
+
+    printf("give us your clientCode :");
+    scanf("%d",&clientCode);
+
+    printf("give us your businessName :");
+    scanf("%s",&businessName);
+
+
+    printf("give us productCode and when y ar finished type '-1' :");
+    scanf("%d",&productCode);
+    while(productCode>=0){
+        printf("give us your quantify :");
+        scanf("%d",&quantify);
+        printf("give us your emergencyNumber for that order :");
+        scanf("%d",&first);
+
+
+        printf("give us productCode and when y ar finished type '-1' :");
+        scanf("%d",&productCode); 
+    }
+
+}
