67911d9c4c8868f5d297ddf1e84deeaff5a00bd7
kwstasbel2006-ship-it <kwstabel2006@gmail.com>
2025-12-21 15:39:06 +0200

    exe

--- dd_prokect/dd_executionorders.c
+++ dd_prokect/dd_executionorders.c
@@ -2,15 +2,12 @@
 #include"dd1.h"
 
 void makeOrder(orderFIFO **head, orderFIFO **tail){
-    printf("Excecute Order\n");
     order data;
     do
     {
-        printf("got in to the delete while loop\n");
-        data=removeOrderFromFIFO(head,tail);
-        printf("after remuve funcrion\n");
-        print(data);
-
+        data=removeOrderFromFIFO(head,tail);        
+        data=exePrint(data);
+        
     } while ((*head != NULL)&&(*tail!=NULL));
 
 }
@@ -54,4 +51,33 @@ void print(order Order){
         printf("\n- %s  (%d) * %lf  =  %lf \n",products[i].productName,temp2->quantify,products[i].productPrice,(products[i].productPrice*temp2->quantify));
         temp2 = temp2->next;
     }
+}
+
+
+order exePrint(order Order){
+    order* temp=&Order;
+    printf("#----Client code=%d ----#\n",temp->clientCode);
+    printf("#----Business name=%s ----#\n\n  --- PRODUCTS ---\n",temp->businessName);
+    int i;
+	orderProducts *temp2=temp->productList;
+    while (temp2 != NULL) {
+        /*printf("-Product code=%s\n", temp2->productCode1);
+        printf("-Quantify=%d\n", temp2->quantify);
+        printf("-First=%d\n", temp2->first);*/
+        for(i=0;i<MAX_PRODUCTS;i++){
+            if (strcmp(products[i].ean ,temp2->productCode1)==0){
+                break;
+            }
+        }
+        if(products[i].productVariable>=temp2->quantify){
+            products[i].productVariable-=temp2->quantify;
+            printf("\n- %s  (%d) * %lf  =  %lf \n",products[i].productName,temp2->quantify,products[i].productPrice,(products[i].productPrice*temp2->quantify));
+        }else{
+            printf("\n--There is not enough stock for product %s. Available stock: %d, requested: %d but we are gonna give y what we have, all the Available stock\n",products[i].productName,products[i].productVariable,temp2->quantify);
+            printf("- %s  (%d) * %lf  =  %lf \n",products[i].productName,products[i].productVariable,products[i].productPrice,(products[i].productPrice*products[i].productVariable));
+            products[i].productVariable=0;
+        }
+        temp2 = temp2->next;
+    }
+    return *temp;
 }
